97487ae87258e58424a71f2012e1cc63b8058394
Damián Silvani <munshkr@gmail.com>
2017-09-21 19:59:35 -0300

    Arregla typo
    find -iname no es case insensitive...?

--- script/run_grass.sh
+++ script/run_grass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Le pasa un archivo descargado cualquiera para configurar el CRS del mapset de GRASS
-somefile=$(find data/ -iname *.tif -print -quit)
+somefile=$(find data/ -name *.TIF -print -quit)
 # Levanta un contenedor de Docker con la imagen de GRASS
 docker run -it --rm -v $(pwd)/script:/script -v $(pwd)/data:/data dymaxionlabs/grass -text -c /$somefile /tmp/db
