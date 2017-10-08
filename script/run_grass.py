e9081f318ee5752a7c9aadf1e04c2a51c32017eb
Damián Silvani <munshkr@gmail.com>
2017-10-08 10:41:01 -0300

    Usa archivo como referencia al crear sesión de GRASS

--- script/run_grass.py
+++ script/run_grass.py
@@ -3,6 +3,7 @@
 import os
 from subprocess import run
 import argparse
+import glob
 
 parser = argparse.ArgumentParser(
         description='Ejecuta GRASS en un container de Docker',
@@ -15,14 +16,20 @@ args = parser.parse_args()
 
 curdir = os.path.dirname(os.path.realpath(__file__))
 datadir = os.path.realpath(args.input_dir)
-datadir_dst = os.path.join('/data', args.input_dir)
+datadir_dst = os.path.join('/', args.input_dir)
+
+files = glob.glob(os.path.join(args.input_dir, '*', '*', '*', '*', '*.TIF'))
+if not files:
+    print('No hay archivos .TIF en {}'.format(args.input_dir))
+some_tif_file = files[0]
 
 cmd = 'docker run -it --rm ' \
       '-v {scriptdir_src}:/script ' \
       '-v {datadir_src}:{datadir_dst} ' \
-      'dymaxionlabs/grass -text -c epsg:32620 /tmp/db'.format(
+      'dymaxionlabs/grass -text -c /{tif_file} /tmp/db'.format(
               scriptdir_src=curdir,
               datadir_src=datadir,
-              datadir_dst=datadir_dst)
+              datadir_dst=datadir_dst,
+              tif_file=some_tif_file)
 
 run(cmd, shell=True)
